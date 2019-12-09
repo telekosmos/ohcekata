4f1e1fad4819207854fa74dab2244eb3546b8af8
telekosmos <dar.es.balat@gmail.com>
2019-12-09 22:47:00 +0100

    Tests for reverse, isPalindrome

--- helper_test.go
+++ helper_test.go
@@ -9,28 +9,49 @@ func TestIsNotPalindrome(t *testing.T) {
 	noPalindrome := "nothing"
 	ans := isPalindrome(noPalindrome)
 	if ans == true {
-		t.Errorf("%t is not a palindrome", ans)
+		t.Errorf("%s is not a palindrome", noPalindrome)
 	}
 }
 
 func TestIsPalindromeEvenTerm(t *testing.T) {
-
+	palindrome := "aabbaa"
+	ans := isPalindrome(palindrome)
+	if ans == false {
+		t.Errorf("%s is a palindrome", palindrome)
+	}
 }
 
 func TestIsPalindromeOddTerm(t *testing.T) {
-
+	palindrome := "aerea"
+	ans := isPalindrome(palindrome)
+	if ans == false {
+		t.Errorf("%s is a palindrome", palindrome)
+	}
 }
 
 func TestIsEmptyPalindrome(t *testing.T) {
-
+	palindrome := ""
+	ans := isPalindrome(palindrome)
+	if ans == false {
+		t.Errorf("%s is a palindrome", palindrome)
+	}
 }
 
 func TestReverse(t *testing.T) {
-
+	sample := "sample"
+	expected := "elpmas"
+	ans := reverse(sample)
+	if ans != expected {
+		t.Errorf("%s not the reverse of %s (%s)", ans, sample, expected)
+	}
 }
 
 func TestReversePalindrome(t *testing.T) {
-
+	sample := "aerea"
+	ans := reverse(sample)
+	if sample != ans {
+		t.Errorf("%s is not the reverse of %s", ans, sample)
+	}
 }
 
 func TestAnswerMorning(t *testing.T) {
